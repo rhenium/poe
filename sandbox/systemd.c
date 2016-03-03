6ed103accf43fd32ca926a2c2ccad85a44ab109f
Kazuki Yamaguchi <k@rhe.jp>
2016-03-03 18:16:31 +0900

    sandbox: fix error handling

--- sandbox/systemd.c
+++ sandbox/systemd.c
@@ -12,23 +12,22 @@ poe_init_systemd(pid_t pid)
 {
     NONNEGATIVE(asprintf(&unit, "poe-sandbox-%d.scope", pid));
 
-#define test(val) if((val) == -1) ERROR("sd_bus function failed")
-    test(sd_bus_open_system(&connection));
+    NONNEGATIVE(sd_bus_open_system(&connection));
 
     sd_bus_message *message = NULL;
-    test(sd_bus_message_new_method_call(connection, &message, sdbus_name, sdbus_path, sdbus_manager, "StartTransientUnit"));
+    NONNEGATIVE(sd_bus_message_new_method_call(connection, &message, sdbus_name, sdbus_path, sdbus_manager, "StartTransientUnit"));
 
-    test(sd_bus_message_append(message, "ss", unit, "fail"));
-    test(sd_bus_message_open_container(message, 'a', "(sv)"));
-    test(sd_bus_message_append(message, "(sv)", "PIDs",                 "au", 1, pid));
-    test(sd_bus_message_append(message, "(sv)", "Description",          "s", unit));
-    test(sd_bus_message_append(message, "(sv)", "MemoryLimit",          "t", POE_MEMORY_LIMIT));
-    test(sd_bus_message_append(message, "(sv)", "TasksMax",             "t", POE_TASKS_LIMIT));
-    test(sd_bus_message_append(message, "(sv)", "CPUShares",            "t", 512ULL));
-    test(sd_bus_message_append(message, "(sv)", "BlockIOWeight",        "t", 10ULL));
-    test(sd_bus_message_append(message, "(sv)", "DevicePolicy",         "s", "strict"));
-    test(sd_bus_message_close_container(message));
-    test(sd_bus_message_append(message, "a(sa(sv))", 0));
+    NONNEGATIVE(sd_bus_message_append(message, "ss", unit, "fail"));
+    NONNEGATIVE(sd_bus_message_open_container(message, 'a', "(sv)"));
+    NONNEGATIVE(sd_bus_message_append(message, "(sv)", "PIDs",                 "au", 1, pid));
+    NONNEGATIVE(sd_bus_message_append(message, "(sv)", "Description",          "s", unit));
+    NONNEGATIVE(sd_bus_message_append(message, "(sv)", "MemoryLimit",          "t", POE_MEMORY_LIMIT));
+    NONNEGATIVE(sd_bus_message_append(message, "(sv)", "TasksMax",             "t", POE_TASKS_LIMIT));
+    NONNEGATIVE(sd_bus_message_append(message, "(sv)", "CPUShares",            "t", 512ULL));
+    NONNEGATIVE(sd_bus_message_append(message, "(sv)", "BlockIOWeight",        "t", 10ULL));
+    NONNEGATIVE(sd_bus_message_append(message, "(sv)", "DevicePolicy",         "s", "strict"));
+    NONNEGATIVE(sd_bus_message_close_container(message));
+    NONNEGATIVE(sd_bus_message_append(message, "a(sa(sv))", 0));
 
     sd_bus_error error = SD_BUS_ERROR_NULL;
     int rc = sd_bus_call(connection, message, 0, &error, NULL);
@@ -39,12 +38,11 @@ poe_init_systemd(pid_t pid)
     // unit が設定できたか？
     while (true) {
         char *gunit;
-        test(sd_pid_get_unit(pid, &gunit));
+        NONNEGATIVE(sd_pid_get_unit(pid, &gunit));
         bool cmp = strcmp(unit, gunit);
         free(gunit);
         if (!cmp) break;
     }
-#undef test
 }
 
 void
@@ -59,4 +57,7 @@ poe_exit_systemd(void)
     sd_bus_error error = SD_BUS_ERROR_NULL;
     sd_bus_call(connection, message, 0, &error, NULL); // ignore error
     sd_bus_message_unref(message);
+
+    sd_bus_unref(connection);
+    free(unit);
 }
