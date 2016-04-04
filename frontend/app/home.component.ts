88e81f4b3fc5286f45c7928130095f23cecd1c41
Kazuki Yamaguchi <k@rhe.jp>
2016-04-04 19:44:41 +0900

    frontend: angular2-polyfills.js の zone.js が古くてうまく動かないので自分で

--- frontend/app/home.component.ts
+++ frontend/app/home.component.ts
@@ -5,14 +5,6 @@ import {EditingData, EditingDataService} from "./editing-data.service";
 
 @Component({
   template: `
-    <h1>ぽえってなに</h1>
-    <p>同じコードを複数のバージョンの処理系で同時に実行できるオンラインコンパイラみたいなやつ</p>
-    <h1>環境</h1>
-    <ul>
-    <li>OS: GNU/Linux x86_64</li>
-    <li>メモリ制限: 128MB</li>
-    <li>時間制限: 5 秒</li>
-    </ul>
   `,
 })
 export class HomeComponent {
